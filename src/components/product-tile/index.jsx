0a215ecb066c9ccf6e216aa7e8d2290abfe3926e
BuickSalesman <adam.odonnell.e@gmail.com>
2024-06-17 17:55:29 -0400

    fixed remove from cart button

--- src/components/product-tile/index.jsx
+++ src/components/product-tile/index.jsx
@@ -1,11 +1,17 @@
-import { useDispatch } from "react-redux";
-import { addToCart } from "../../store/slices/cart-slice";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../../store/slices/cart-slice";
 
 export default function ProductTile({ product }) {
   const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state);
 
   function handleAddTocart() {
     dispatch(addToCart(product));
+  }
+
+  function handleRemoveFromCart() {
+    console.log("removed");
+    dispatch(removeFromCart(product.id));
   }
 
   return (
@@ -18,8 +24,11 @@ export default function ProductTile({ product }) {
           <h1 className="w-40 truncate mt-3 text-grey-700 font-bold text-lg">{product?.title}</h1>
         </div>
         <div className="flex intems-center justify-center w-full mt-5">
-          <button onClick={handleAddTocart} className="bg-red-950 text-white border-2 rounded-lg font-bold p-4">
-            Add to cart
+          <button
+            onClick={cart.some((item) => item.id === product.id) ? handleRemoveFromCart : handleAddTocart}
+            className="bg-red-950 text-white border-2 rounded-lg font-bold p-4"
+          >
+            {cart.some((item) => item.id === product.id) ? "Remove from cart" : "Add to cart"}
           </button>
         </div>
       </div>
